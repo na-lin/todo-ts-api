4534b9dca8deb7ed81b867946540e6e546ee6eae
Na Lin <lna483018@gmail.com>
2023-04-18 18:43:27 -0400

    Integrating TypeORM

--- index.ts
+++ index.ts
@@ -1,13 +1,33 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 
 const app: Express = express();
 dotenv.config();
+
+// create Database Connection
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("express + typescript!");
 });
 
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
 app.listen(process.env.PORT, () => {
   console.log("Listen on port 3001");
 });
