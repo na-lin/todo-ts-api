a3c723d06fe1ce26808c3aa628f3399030322ed7
Na Lin <lna483018@gmail.com>
2023-04-18 19:45:05 -0400

    create a task TypeORM repository

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,7 @@
 import { Task } from "./tasks.entity";
-
+import { AppDataSource } from "../..";
 export class TasksController {
+  constructor(private taskRepository = AppDataSource.getRepository(Task)) {}
+
   public async getAllTasks(): Promise<Task[]> {}
 }
