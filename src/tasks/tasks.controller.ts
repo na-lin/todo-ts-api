b9ab0f84c37054a19535e6ef11a60eae59c41a9f
Na Lin <lna483018@gmail.com>
2023-04-18 20:20:40 -0400

    enable getAllTasks route and controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,27 @@
-import { Task } from "./tasks.entity";
 import { AppDataSource } from "../..";
+import { Task } from "./tasks.entity";
+import { instanceToPlain } from "class-transformer";
+
 export class TasksController {
   constructor(private taskRepository = AppDataSource.getRepository(Task)) {}
 
-  public async getAllTasks(): Promise<Task[]> {}
+  public async getAllTasks(): Promise<Task[]> {
+    // declare a variable to hold all tasks
+    let allTasks: Task[];
+    try {
+      // fetch all task using the repository
+      allTasks = await this.taskRepository.find({
+        order: {
+          date: "ASC",
+        },
+      });
+      console.log(allTasks);
+      // convert the task instance to an array of object
+      allTasks = instanceToPlain(allTasks) as Task[];
+      return allTasks;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
 }
--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,7 +1,14 @@
 import { Router, Request, Response } from "express";
+import { TasksController } from "./tasks.controller";
 
 export const tasksRouter: Router = Router();
 
-tasksRouter.get("/", (req: Request, res: Response) => {
-  res.send("from task router");
+tasksRouter.get("/", async (req: Request, res: Response) => {
+  const taskController = new TasksController();
+  try {
+    const allTasks = await taskController.getAllTasks();
+    res.status(200).json(allTasks);
+  } catch (err) {
+    console.log("Task router get all tasks", err);
+  }
 });
